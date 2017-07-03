53b09228ef4517bd53d7895b8c5ff490182a4f30
Htinel <3134ant@gmail.com>
2017-07-03 09:04:16 +0300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,11 @@
-New
+require 'bundler/setup'
+require 'codebreaker'
+require 'codebreaker/console_cb'
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
